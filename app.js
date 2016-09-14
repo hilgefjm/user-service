5e0484f8101b84dc3a4a85e7b296340d8b78879c
hilgefjm <hilgefjm@mail.uc.edu>
2016-09-14 19:08:16 -0400

    Linted files

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 8080;
-var ENV = process.env.ENV || 'dev';
+var config = require('./config/config.js');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 var logger = require('./lib/logger');
-app.use(require('morgan')("combined", { "stream": logger.stream }));
+app.use(require('morgan')('combined', { 'stream': logger.stream }));
 
 logger.info('Registering Homepage service at /');
 var index = require('./routes/index');
@@ -23,17 +22,20 @@ logger.info('Registering Name service at /name');
 var name = require('./routes/name');
 app.use('/name', name);
 
-logger.info('Registering User service at /login');
+logger.info('Registering Login service at /login');
+var loginService = require('./routes/login');
+app.use('/login', loginService);
+
 logger.info('Registering User service at /create-user');
 var userService = require('./routes/user');
 app.use('/user', userService);
 
-app.use(function(req, res, next){
-	var err = new Error('Not Found');
-	err.status = 404;
-	res.status(err.status).json({ http_status: { status: err.status, message: err.message } });
-	next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(err.status).json({ http_status: { status: err.status, message: err.message } });
+  next(err);
 });
 
-app.listen(PORT);
-logger.info('Server running on port ' + PORT);
+app.listen(config.port);
+logger.info('Server running on port ' + config.port);
